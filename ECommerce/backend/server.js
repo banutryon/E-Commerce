224e2b2b108a5c646ebfd36518c513991c8ce44b
banu tryon <banutryon@banus-mbp.lan>
2021-03-31 14:45:46 -0700

    bug fixing

--- ECommerce/backend/server.js
+++ ECommerce/backend/server.js
@@ -1,7 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
 import Data from "./Data.js";
+import userRouter from "./routers/userRouter.js";
 
 const app = express();
+mongoose.connect("mongodb://localhost/ecommerce", {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
+});
+app.use("/api/users", userRouter);
 app.get("/api/products/:id", (req, res) => {
 	const product = Data.products.find((x) => x._id === req.params.id);
 	if (product) {
@@ -10,11 +18,16 @@ app.get("/api/products/:id", (req, res) => {
 		res.status(404).send({ message: "Product not found" });
 	}
 });
+
 app.get("/api/products", (req, res) => {
 	res.send(Data.products);
 });
+
 app.get("/", (req, res) => {
 	res.send("Server is ready");
+});
+app.use((err, req, res, next) => {
+	res.status(500).send({ message: err.message });
 });
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
