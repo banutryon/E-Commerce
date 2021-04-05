8ffabf5e8b02c3f1120c29265e09f66dec97f3dc
banu tryon <banutryon@gmail.com>
2021-04-04 18:21:20 -0700

    added order router.js file

--- ECommerce/backend/utils.js
+++ ECommerce/backend/utils.js
@@ -13,3 +13,19 @@ export const generateToken = (user) => {
 		}
 	);
 };
+
+export const isAuth = (req, res, next) => {
+	const authorization = req.headers.authorization;
+	if (authorization) {
+		const token = authorization.slice(7, authorization.lenght);
+		jwt.verify(token, process.env.JWT_SECRET || "seceret", (err, decode) => {
+			if (err) {
+				res.status(401).send({ message: "Invalid Token" });
+			} else {
+				req.user = decode;
+			}
+		});
+	} else {
+		res.status(401).send({ message: "No Token" });
+	}
+};
