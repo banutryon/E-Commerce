40c4301eae7414e402e159da4798b7f511f04354
banu tryon <banutryon@gmail.com>
2021-04-06 14:34:07 -0700

    added order history

--- ECommerce/frontend/src/App.js
+++ ECommerce/frontend/src/App.js
@@ -11,6 +11,7 @@ import ShippingAddressScreen from "./screens/ShippingAddressScreen";
 import PaymentMethodScreen from "./screens/PaymentMethodScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import OrderScreen from "./screens/OrderScreen";
+import OrderHistoryScreen from "./screens/OrderHistoryScreen";
 function App() {
 	const cart = useSelector((state) => state.cart);
 	const { cartItems } = cart;
@@ -43,9 +44,14 @@ function App() {
 									{""} <i className="fa fa-caret-down"></i>
 								</Link>
 								<ul className="dropdown-content">
-									<Link to="#signout" onClick={signoutHandler}>
-										Sign Out
-									</Link>
+									<li>
+										<Link to="/orderhistory">Order History</Link>
+									</li>
+									<li>
+										<Link to="#signout" onClick={signoutHandler}>
+											Sign Out
+										</Link>
+									</li>
 								</ul>
 							</div>
 						) : (
@@ -62,6 +68,7 @@ function App() {
 					<Route path="/payment" component={PaymentMethodScreen}></Route>
 					<Route path="/placeorder" component={PlaceOrderScreen}></Route>
 					<Route path="/order/:id" component={OrderScreen}></Route>
+					<Route path="/orderhistory" component={OrderHistoryScreen}></Route>
 					<Route path="/" component={HomeScreen} exact></Route>
 				</main>
 				<footer>All right reserved</footer>
