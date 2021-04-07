6fdbbf4ff3db225ff7ceed2631440004da1bd64c
banu tryon <banutryon@gmail.com>
2021-04-07 14:07:15 -0700

    added admin products page

--- ECommerce/frontend/src/App.js
+++ ECommerce/frontend/src/App.js
@@ -14,6 +14,8 @@ import OrderScreen from "./screens/OrderScreen";
 import OrderHistoryScreen from "./screens/OrderHistoryScreen";
 import ProfileScreen from "./screens/ProfileScreen";
 import PrivateRoute from "./components/PrivateRoute";
+import ProductListScreen from "./screens/ProductListScreen";
+import AdminRoute from "./components/AdminRoute";
 function App() {
 	const cart = useSelector((state) => state.cart);
 	const { cartItems } = cart;
@@ -99,6 +101,10 @@ function App() {
 						path="/profile"
 						component={ProfileScreen}
 					></PrivateRoute>
+					<AdminRoute
+						path="/productlist"
+						component={ProductListScreen}
+					></AdminRoute>
 					<Route path="/" component={HomeScreen} exact></Route>
 				</main>
 				<footer>All right reserved</footer>
